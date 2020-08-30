da8ac32598cf474f0b28f363355d21e96b6808ae
Sue Liu <39272585+yusueliu@users.noreply.github.com>
2020-08-30 08:48:00 +0100

    mv unzipped file to correct location

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/random_forest_model/random_forest_model/datasets/
+kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/random_forest_model/random_forest_model/datasets/ 
+unzip -o packages/random_forest_model/random_forest_model/datasets/*.zip 
+mv test.csv train.csv packages/random_forest_model/random_forest_model/datasets/
